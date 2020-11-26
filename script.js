f5c0bbec3abcf2789ff9d7051f1d7b5fbe92c6d4
Sianmin <jeinu95@gmail.com>
2020-11-26 22:31:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -122,10 +122,13 @@ for (var i = 0; i < model_n; i++) {
     var enter_tween = TweenMax.to(`#canvas${i+1}`, 1, {
         opacity: 1,
         onUpdate: function (model_name) {
-            var cnvs = documnet.getElementById(`canvas${model_name}`);
-            var ctx = cnvs.getContext('2d');
-            ctx.clearRect(0, 0, cnvs.width, cnvs.height);
-            ctx.beginPath();
+            if (model_name >= 1) {
+                var cnvs = documnet.getElementById(`canvas${model_name}`);
+                var ctx = cnvs.getContext('2d');
+                ctx.clearRect(0, 0, cnvs.width, cnvs.height);
+                ctx.beginPath();
+            }
+
             focused_canvas = model_name + 1;
             video.currentTime = ((frame_per_model * model_name)) * time_per_frame;
         },
