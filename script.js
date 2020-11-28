d89707fd12fcebd01a0d7490892ae1b6e023ce85
Sianmin <jeinu95@gmail.com>
2020-11-28 22:19:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function resize() {
 
 function redraw() {
     hidden_context.drawImage(video, ox, oy, w, h);
-    focused_canvas.drawImage(hidden_canvas, ox, oy, w, h);
+    focused_canvas.drawImage(hidden_canvas, 0, 0);
     video.pause();
     window.requestAnimationFrame(redraw);
 }
