b8f083645e4880726cc3f48830b842375093fbfb
Sianmin <jeinu95@gmail.com>
2020-11-29 17:19:08 +0900

    replace video with canvas

--- script.js
+++ script.js
@@ -1,92 +1,82 @@
-window.onerror = function (msg, url, linenumber) {
-    alert('Error message: ' + msg + '\nURL: ' + url + '\nLine Number: ' + linenumber);
-    return true;
-}
-const mp4_fat = document.currentScript.getAttribute('mp4_fat');
-const mp4_tall = document.currentScript.getAttribute('mp4_tall');
-const model_n = document.currentScript.getAttribute('model_n');
+var mp4_fat = document.currentScript.getAttribute('mp4_fat');
+var mp4_tall = document.currentScript.getAttribute('mp4_tall');
+var model_n = document.currentScript.getAttribute('model_n');
 
 const time_per_frame = 0.04,
     frame_per_model = 250;
 let wx = window.innerWidth,
     wy = window.innerHeight,
-    w, h, ox, oy,
-    lastScrollTop = 0;
-const minimumPlaybackRate = 0.1;
-let video_mp4_url = wx / wy >= 1920 / 1080 * 0.6 ? mp4_fat : mp4_tall;
-let ratio = wx / wy >= 1920 / 1080 * 0.6 ? 1920 / 1080 : 1080 / 1920;
-let curTime;
-let video_array = new Array(model_n);
+    w, h, ox, oy;
+let lastScrollTop = 0;
+const minimumPlaybackRate = 0.0625;
 
 function scrollTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-scrollTop();
 
-// Create video tag
-for (var i = 0; i < model_n; i++) {
-    document.body.innerHTML += `<video id="video${i}" class="container" crossorigin="anonymous" playsinline autoplay muted></video>`;
+scrollTop();
+document.body.style.overflow = "hidden";
+
+// Create canvas and canvas
+for (var i = 1; i <= model_n; i++) {
+    document.body.innerHTML += `<canvas id="canvas${i}" class="container" margin:="unset"></canvas>`;
 }
-for (var i = 0; i < model_n; i++) {
-    video_array[i] = document.getElementById(`video${i}`);
-}
+
+
+let video_mp4_url = wx / wy >= 1920 / 1080 * 0.6 ? mp4_fat : mp4_tall;
+let ratio = wx / wy >= 1920 / 1080 * 0.6 ? 1920 / 1080 : 1080 / 1920;
+let video = document.getElementById('video');
+let curTime;
 
 // Video Loading
-function videoloading() {
-    var req = new XMLHttpRequest();
-    req.addEventListener("progress", function (evt) {
-        scrollTop();
-        if (evt.lengthComputable) {
-            var percentComplete = evt.loaded / evt.total;
-            var downloaed = parseInt(evt.loaded / 1024 / 1024);
-            var total = parseInt(evt.total / 1024 / 1024);
-            document.getElementById('loading_text').innerHTML = `${parseInt(percentComplete*100)}% Loading<br>${downloaed}MB / ${total}MB`;
-            if (percentComplete >= 1) {
-                loading_end();
-            }
-        }
-    }, true);
-    req.open('GET', video_mp4_url, true);
-    req.responseType = 'blob';
-    req.onload = function () {
-        if (this.status === 200) {
-            var videoBlob = this.response;
-            var vid = URL.createObjectURL(videoBlob);
-            for (var i = 0; i < model_n; i++) {
-                video_array[i].src = vid;
-            }
+var req = new XMLHttpRequest();
+req.addEventListener("progress", function (evt) {
+    scrollTop();
+    if (evt.lengthComputable) {
+        var percentComplete = evt.loaded / evt.total;
+        var downloaed = parseInt(evt.loaded / 1024 / 1024);
+        var total = parseInt(evt.total / 1024 / 1024);
+        document.getElementById('loading_text').innerHTML = `${parseInt(percentComplete*100)}% Loading<br>${downloaed}MB / ${total}MB`;
+
+        if (percentComplete >= 1) {
+            loading_end();
         }
     }
-    req.send();
+}, true);
+req.open('GET', video_mp4_url, true);
+req.responseType = 'blob';
+req.onload = function () {
+    if (this.status === 200) {
+        var videoBlob = this.response;
+        var vid = URL.createObjectURL(videoBlob);
+        video.src = vid;
+    }
 }
+req.send();
 
 function loading_end() {
     resize();
-    for (var i = 0; i < model_n; i++) {
-        video_array[i].defaultPlaybackRate = minimumPlaybackRate;
-        video_array[i].playbackRate = minimumPlaybackRate;
-        video_array[i].play();
-    }
-
+    video.defaultPlaybackRate = minimumPlaybackRate;
+    video.playbackRate = minimumPlaybackRate;
+    video.play();
     document.body.style.overflow = 'visible';
     document.getElementById('loading_logo').style.display = 'none';
     document.getElementById('loading_text').style.display = 'none';
-    // window.addEventListener('scroll', function () {
-    //     var st = window.pageYOffset;
-    //     if (st > lastScrollTop) {
-    //         focused_video.defaultPlaybackRate = minimumPlaybackRate;
-    //         focused_video.playbackRate = minimumPlaybackRate;
-    //     } else {
-    //         focused_video.defaultPlaybackRate = 0;
-    //         focused_video.playbackRate = 0;
-    //     }
-    //     lastScrollTop = st <= 0 ? 0 : st;
-    // }, false);
+    window.addEventListener('scroll', function () {
+        var st = window.pageYOffset;
+        if (st > lastScrollTop) {
+            video.defaultPlaybackRate = minimumPlaybackRate;
+            video.playbackRate = minimumPlaybackRate;
+        } else {
+            video.defaultPlaybackRate = 0;
+            video.playbackRate = 0;
+        }
+        lastScrollTop = st <= 0 ? 0 : st;
+    }, false);
     window.addEventListener('resize', resize, false);
-    scroll_ani_start();
+    window.requestAnimationFrame(redraw);
 }
-videoloading();
 
 function resize() {
     wx = window.innerWidth, wy = window.innerHeight;
@@ -98,98 +88,103 @@ function resize() {
         ox = 0, oy = (wy - h) / 2;
     }
 
-    for (var i = 0; i < model_n; i++) {
-        var video = video_array[i];
-        video.width = wx;
-        video.height = wy;
-        video.style.margin = 0;
+    for (var i = 1; i <= model_n; i++) {
+        var canvas = document.getElementById(`canvas${i}`);
+        canvas.width = wx;
+        canvas.height = wy;
+        canvas.style.margin = 0;
     }
 }
 
-function scroll_ani_start() {
-    const per_enter_duration = document.documentElement.clientHeight * 0.5,
-        per_center_duration = document.documentElement.clientHeight * 4,
-        per_leave_duration = document.documentElement.clientHeight * 0.5;
+function redraw() {
+    focused_canvas.drawImage(video, ox, oy, w, h);
+    window.requestAnimationFrame(redraw);
+}
 
-    // init controller
-    let controller = new ScrollMagic.Controller({
-        globalSceneOptions: {
-            triggerHook: 0
-        }
+/*
+ * Scroll Animation
+ */
+const per_enter_duration = document.documentElement.clientHeight * 0.5,
+    per_center_duration = document.documentElement.clientHeight * 4,
+    per_leave_duration = document.documentElement.clientHeight * 0.5;
+
+// init controller
+let controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+        triggerHook: 0
+    }
+});
+
+// TweenMax can tween any property of any object. We use this object to cycle through the array
+let currs = new Array(model_n);
+let focused_canvas = document.getElementById(`canvas1`).getContext('2d');
+
+for (var i = 0; i < model_n; i++) {
+    currs[i] = {
+        cur_frame: 0
+    }
+
+    // create tween
+    var enter_tween = TweenMax.to(`#canvas${i+1}`, 1, {
+        opacity: 1,
+        onUpdate: function (model_name) {
+            focused_canvas = document.getElementById(`canvas${model_name + 1}`).getContext('2d');
+            curTime = frame_per_model * model_name * time_per_frame;
+            video.currentTime = curTime;
+        },
+        onUpdateParams: [i]
     });
 
-    // TweenMax can tween any property of any object. We use this object to cycle through the array
-    let currs = new Array(model_n);
+    var center_tween = TweenMax.to(currs[i], 1, {
+        cur_frame: frame_per_model - 1,
+        roundProps: "cur_frame",
+        repeat: 0,
+        immediateRender: true,
+        ease: Linear.easeNone, // show every image the same ammount of time
+        onUpdate: function (model_name) {
+            focused_canvas = document.getElementById(`canvas${model_name + 1}`).getContext('2d');
+            curTime = (frame_per_model * model_name + currs[model_name].cur_frame) * time_per_frame;
+            video.currentTime = curTime;
+        },
+        onUpdateParams: [i]
+    });
 
-    for (var i = 0; i < model_n; i++) {
-        currs[i] = {
-            cur_frame: 0
-        }
+    var leave_tween = TweenMax.to(`#canvas${i+1}`, 1, {
+        opacity: 0,
+    });
 
-        // create tween
-        var enter_tween = TweenMax.to(`#video${i}`, 1, {
-            opacity: 1,
-            onUpdate: function (model_name) {
-                try {
-                    focused_video = video_array[model_name];
-                    curTime = frame_per_model * model_name * time_per_frame;
-                    focused_video.currentTime = curTime;
-                } catch {
-                    return true;
-                }
-            },
-            onUpdateParams: [i]
-        });
+    // enter canvas
+    var enter_scene = new ScrollMagic.Scene({
+            triggerElement: `#canvas${i+1}`,
+            triggerHook: "onEnter",
+            offset: -per_enter_duration,
+            duration: per_enter_duration,
+        })
+        .setTween(enter_tween)
+        .addTo(controller);
 
-        var center_tween = TweenMax.to(currs[i], 1, {
-            cur_frame: frame_per_model - 1,
-            roundProps: "cur_frame",
-            repeat: 0,
-            immediateRender: true,
-            ease: Linear.easeNone,
-            onUpdate: function (model_name) {
-                try {
-                    focused_video = video_array[model_name];
-                    curTime = (frame_per_model * model_name + currs[model_name].cur_frame) * time_per_frame;
-                    focused_video.currentTime = curTime;
-                } catch {
-                    return true;
-                }
-            },
-            onUpdateParams: [i]
-        });
+    // center canvas
+    var center_scene = new ScrollMagic.Scene({
+            triggerElement: `#canvas${i+1}`,
+            duration: per_center_duration,
+        })
+        .setPin(`#canvas${i+1}`)
+        .setTween(center_tween)
+        .addTo(controller);
 
-        var leave_tween = TweenMax.to(`#video${i}`, 1, {
-            opacity: 0,
-        });
+    // leave canvas
+    var leave_scene = new ScrollMagic.Scene({
+            triggerElement: `#canvas${i+1}`,
+            triggerHook: "onLeave",
+            offset: per_center_duration,
+            duration: per_leave_duration,
+        })
+        .setTween(leave_tween)
+        .addTo(controller);
 
-        // enter video
-        var enter_scene = new ScrollMagic.Scene({
-                triggerElement: `#video${i}`,
-                triggerHook: "onEnter",
-                offset: -per_enter_duration,
-                duration: per_enter_duration,
-            })
-            .setTween(enter_tween)
-            .addTo(controller);
 
-        // center video
-        var center_scene = new ScrollMagic.Scene({
-                triggerElement: `#video${i}`,
-                duration: per_center_duration,
-            })
-            .setPin(`#video${i}`)
-            .setTween(center_tween)
-            .addTo(controller);
 
-        // leave video
-        var leave_scene = new ScrollMagic.Scene({
-                triggerElement: `#video${i}`,
-                triggerHook: "onLeave",
-                offset: per_center_duration,
-                duration: per_leave_duration,
-            })
-            .setTween(leave_tween)
-            .addTo(controller);
-    }
+
+
+
 }
